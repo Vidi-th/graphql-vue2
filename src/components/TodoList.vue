deebd6e4cab81a7ba4b5783991489bb8fafe02a9
vidityar adith <van.adith@gmail.com>
2022-04-23 16:46:27 +0700

    Update GraphQL Query praktikum

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,37 +4,111 @@
         <input type="text" v-model= "search" placeholder="todo-search" >
         <!-- <button @click="OnSearch"> Search Todo </button> -->
       </div>
-      <div v-for="todo in todoList_by_pk" :key="todo.id">
+      <!-- <div v-for="todo in todoList_by_pk" :key="todo.id">
         {{ todo.title }}
-      </div>
+      </div> -->
+
+      <hr>
+
+      <ApolloQuery 
+      :query="search!=''?(gql => gql`
+        query MyQuery4($id: Int!) {
+          todoList_by_pk(id: $id) {
+            title
+            id
+          }
+        }
+      `) : (gql =>gql`query list{
+          todoList{
+            title
+            id
+          }
+        }`)"
+      
+      :variables="searchComputed"
+      >
+      <template v-slot="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
+
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occurred
+          {{ error }}
+        </div>
+
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">
+          <div v-if="data.todoList_by_pk">{{ data.todoList_by_pk.title }}</div>
+          <div v-else-if="data.todoList_by_pk == null && !data.todoList">{{ "Belum ada todo" }}</div>
+          <div v-if="data.todoList">
+            <div v-for="todo in data.todoList" :key="todo.id">
+              {{ todo.title }}
+            </div>
+          </div>
+        </div>
+
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
+
+      <!-- <div v-for="todo in todoListComputed" :key="todo.id">
+        {{ todo.title }}
+      </div> -->
+
     </div>
 </template>
 
 <script>
-import gql from 'graphql-tag'
-export default {
-  apollo:{
-    todoList_by_pk:{
-      query: gql`query MyQuery4($id: Int!) {
-        todoList_by_pk(id: $id) {
-          title
-        }
-      }`,
+// import gql from 'graphql-tag'
 
-      variables() {
-        return {id: this.search}
-      },
+export default {
+  // apollo:{
+  //   todoList_by_pk:{
+  //     query: gql`query MyQuery4($id: Int!) {
+  //       todoList_by_pk(id: $id) {
+  //         title
+  //       }
+  //     }`,
+
+  //     variables() {
+  //       return {id: this.search}
+  //     },
       
-      update(data){
-        return data
-      }
-    }, 
-  },
+  //     update(data){
+  //       return data
+  //     }
+  //   },
+    
+  //   todoList: gql`query list{
+  //     todoList{
+  //       title
+  //     }
+  //   }`,
+  // },
   
   data: () => ({
-    search: "",
+    search: 1,
+    todoList: {},
   }),
 
+  computed:{
+    todoListComputed(){
+      
+      console.log(this.todoList_by_pk, "apa");
+      if(this.search == "" || this.todoList_by_pk == null){
+        return this.todoList;
+      }
+        return this.todoList_by_pk;
+    },
+    searchComputed(){
+      console.log("Search computed :", this.search);
+      if(this.search !== ""){
+        return {id: this.search};
+      }
+      return {};
+    }
+  },
 }
 </script>
 
